da43542131481a85f28c07739f10acb32b5651bf
cristianpie <cristianpie2002@gmail.com>
2019-08-26 17:09:10 -0300

    calculos

---
+++ backend/contas.php
@@ -0,0 +1,80 @@
+
+<?php
+// VOLUME DE AGUA
+$Va0  // VOLUME DE ÁGUA INICIAL
+$Vapg  // VOLUME ABSORVIDO PELOS GRÃOS 
+$Ve = (total/100)*20) // VOLUME EVAPORADO
+$Vl  // VOLUME ÁGUA PARA LAVAGEM DO BAGAÇO 
+$Vf  // VOLUME PRÉ-FERVURA
+$Vp  // VOLUME DE PERDA (Trub + retenção térmica)
+
+
+
+
+
+
+
+
+
+//IBU
+
+$Wg
+$Uporcent
+$Aporcent
+
+
+
+
+// ABV ÁLCOOL
+$DO  // DENSIDADE ORIGINAL (INICIAL)
+$DF  // DENSIDADE FINAL
+
+
+
+$tabela_correção_densidade = array(-0.0016, // GRALS C° | FATOR CORREÇÃO
+                                    -0.0017,
+                                    -0.0017,
+                                    -0.0018,
+                                    -0.0018,
+                                    -0.0018,
+                                    -0.0017,
+                                    -0.0017,
+                                    -0.0016,
+                                    -0.0016,
+                                    -0.0015,//10
+                                    -0.0014,
+                                    -0.0013,
+                                    -0.0012,
+                                    -0.0010,
+                                    -0.0009,
+                                    -0.0008,
+                                    -0.0006,
+                                    -0.0004,
+                                    -0.0002,
+                                    0,      //20
+                                    0.0002,
+                                    0.0004,
+                                    0.0006,
+                                    0.0009, //24
+                                    0.0010,
+                                    0.0014,
+                                    0.0016,
+                                    0.0019,
+                                    0.0022,
+                                    0.0025, //30
+                                    0.0028,
+                                    0.0031,
+                                    0.0034,
+                                    0.0037,
+                                    0.0041,
+                                    0.0044,
+                                    0.0048,
+                                    0.0051,
+                                    0.0055,
+                                    0.0059,
+                                    0.0062);//41                        );
+
+                                    
+                                    
+
+?>
