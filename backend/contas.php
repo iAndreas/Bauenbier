8dc4998e1c3f5fe7c8e3f9c5a7deef8ae0619d41
cristianpie <cristianpie2002@gmail.com>
2019-09-16 15:56:15 -0300

    atualizaaco

--- backend/contas.php
+++ backend/contas.php
@@ -20,23 +20,15 @@ $SG = $massa / $volume;
 
 
  // COR
-$volume_gal = $volume_em_litros * 3,785411784;// CONVERSSÃO DE LITROS PARA GALÃO    
+$volume_gal = $volume_em_litros * 3.785411784;// CONVERSSÃO DE LITROS PARA GALÃO    
 $peso_grãos = ($kgramas * 1000) * 0.0022046;
 
 
 
 
- $UCM = ($peso_grãos/*(em libras)*/ * $cor_grão/*(em graus lovibond)*/) / $volume_gal /*(em galões americano)*/
-
-                                      
-
- 
-
-Para cervejas leves, o UCM apresenta uma boa estimativa de cor SRM, entretanto quando a coloração da cerveja ultrapassa 6-8 SRM, você precisa fazer uma correção utilizando a equação de Morey.
-
- 
-
-SRM = 1,4922 x (UCM 0,6859)
+$UCM = ($peso_grãos/*(em libras)*/ * $cor_grão/*(em graus lovibond)*/) / $volume_gal /*(em galões americano)*/
+ //|\\ Para cervejas leves, o UCM apresenta uma boa estimativa de cor SRM, entretanto quando a coloração da cerveja ultrapassa 6-8 SRM, você precisa fazer uma correção utilizando a equação de Morey.
+$SRM = 1.4922 * ($UCM ** 0.6859);
 
 
 
